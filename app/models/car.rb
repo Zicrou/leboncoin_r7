0d78b45aa3fd2d161e4f429a498fe9258a28453e
Zicrou <aazseck@gmail.com>
2022-01-24 16:58:40 +0000

    Set Action Text And Active Storage, Fixed bugs in description to show image by installin this commandd sudo apt install imagemagick libvips

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,9 @@
 class Car < ApplicationRecord
     validates_presence_of :marque, :modele, :price, :insider_car_image, :front_car_image, :profil_car_image, :distance, :description, :color, :year
+    
+    has_rich_text :description
+    
+    has_one_attached :insider_car_image
+    has_one_attached :front_car_image
+    has_one_attached :profil_car_image
 end
