1ae996f84759e312fdce6332983dd556e26b3154
Zicrou <aazseck@gmail.com>
2022-01-24 16:18:19 +0000

    Added Car Validation Attributes

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,3 @@
 class Car < ApplicationRecord
+    validates_presence_of :marque, :modele, :price, :insider_car_image, :front_car_image, :profil_car_image, :distance, :description, :color, :year
 end
